7a622678392ff0e5c9aa85b50a4557cba14a6fbd
Melinda Golden <info@melindagolden.com>
2023-07-28 17:05:09 -0400

    add: blog post

---
+++ components/blog/BlogPost.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="flex flex-col items-start justify-between">
+    <div class="flex items-center gap-x-4 text-sm">
+      <!-- Date -->
+      <time :datetime="post.date" class="text-gray-500 dark:text-gray-300">
+        {{ formatDate(post.date) }}
+      </time>
+
+      <!-- Tags -->
+      <NuxtLink
+        v-for="tag in post.tags"
+        :key="tag"
+        to="/"
+        class="block rounded-full max-w-fit"
+      >
+        <BaseTag>{{ tag }}</BaseTag>
+      </NuxtLink>
+    </div>
+
+    <div class="group relative">
+      <!-- Title -->
+      <h3
+        class="mt-3 text-xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600 dark:text-white dark:group-hover:text-gray-300"
+      >
+        <NuxtLink :href="`${post._path}/`">
+          <span class="absolute inset-0" />
+          {{ post.title }}
+        </NuxtLink>
+      </h3>
+
+      <!-- Description -->
+      <p
+        class="mt-5 line-clamp-3 text-base leading-8 text-gray-600 dark:text-gray-300"
+      >
+        {{ post.description }}
+      </p>
+    </div>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true
+  }
+});
+
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+};
+</script>
+
+<style scoped></style>
