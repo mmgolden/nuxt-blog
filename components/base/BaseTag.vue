dbc35f0df24071d712c3816fb9f617fc6a87aec5
Melinda Golden <info@melindagolden.com>
2023-07-28 16:43:20 -0400

    add: base tag component

---
+++ components/base/BaseTag.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="relative z-10 rounded-full max-w-fit bg-gray-50 dark:bg-gray-700 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-800"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
