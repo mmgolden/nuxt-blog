33b4f1f8242fd93b50834849fac55cdc41af6d54
Melinda Golden <info@melindagolden.com>
2023-07-28 17:02:01 -0400

    add: page title component

---
+++ components/layout/LayoutPageTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="mb-10 border-b border-gray-200 dark:border-gray-800 pb-10 sm:mb-16 sm:pb-16 bg:white dark:bg-gray-900"
+  >
+    <!-- Page title -->
+    <h1
+      class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl"
+    >
+      <slot name="title" />
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
