c121f0994cdb2acb3385079149088ec938f0236e
Melinda Golden <info@melindagolden.com>
2023-07-28 16:48:07 -0400

    add: copy to hero section

--- components/home/HomeHero.vue
+++ components/home/HomeHero.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="mx-auto container p-6 lg:px-8">
+  <div class="mx-auto max-w-4xl p-6 lg:px-8">
     <img
-      class="h-28 w-auto rounded-full mt-8 mb-6"
+      class="h-28 w-auto rounded-full mt-8 mb-8"
       src="~/assets/png/avatar.png"
       alt="Headshot of Melinda Golden"
     />
-    <h1 class="text-5xl font-bold mb-2 text-gray-900 dark:text-white">
+    <h1 class="text-5xl font-bold mb-4 text-gray-900 dark:text-white">
       Hello, I'm Melinda Golden
     </h1>
-    <p class="text-lg text-gray-600 dark:text-gray-300 lg:max-w-xl">
-      I’m a front-end developer based in Asheville, NC. I specialize in
-      performant, accessible, and responsive web apps. Follow my journey
-      building products.
+    <p class="text-lg text-gray-600 dark:text-gray-300">
+      I’m a self-taught software developer with a fine art and photography
+      background based in Asheville, NC. I have 7+ years of experience in
+      front-end development and use HTML, CSS, JavaScript, TypeScript, and Vue.
+      I run a local group for women in technology. My goal is to share my
+      knowledge and help other developers on their journey.
     </p>
   </div>
 </template>
