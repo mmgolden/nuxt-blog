a84c0c382e3cd28f42649be11cd0793e3c38aa1b
Melinda Golden <info@melindagolden.com>
2024-12-22 21:50:51 -0500

    feat: add base button

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :type="type"
+    class="rounded-full px-6 pt-3 pb-2.5 text-base flex items-center justify-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+    :class="[
+      isPrimary
+        ? 'bg-neutral-900 dark:bg-neutral-800 text-white hover:bg-neutral-700 dark:hover:bg-white/10 focus-visible:outline-neutral-700'
+        : '',
+      isSecondary
+        ? 'bg-neutral-50 dark:bg-neutral-900 text-neutral-900 dark:text-white hover:bg-neutral-100 dark:hover:bg-white/10 ring-1 ring-inset ring-neutral-300 dark:ring-neutral-700 focus-visible:outline-neutral-900 dark:focus-visible:outline-neutral-700'
+        : ''
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  type?: "button" | "submit";
+  primary?: boolean;
+  secondary?: boolean;
+}
+
+const {
+  type = "button",
+  primary = true,
+  secondary = false
+} = defineProps<Props>();
+
+const isPrimary = computed(() => {
+  if (secondary) {
+    return false;
+  }
+
+  return primary;
+});
+
+const isSecondary = computed(() => secondary);
+</script>
+
+<style scoped></style>
